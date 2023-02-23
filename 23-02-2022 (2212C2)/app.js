40db4dc57ee6f9239890c75970a1750b353649c3
Sameer Faisal <45840879+SameerFaisal@users.noreply.github.com>
2023-02-23 13:20:28 +0500

    23-02-2023

---
+++ 23-02-2022 (2212C2)/app.js
@@ -0,0 +1,97 @@
+// document.write(2==="2")  //False
+// document.write(2===2)    //True
+
+// document.write(3!==2)    //True
+
+// document.write(5>2 && 2<3)  //True
+// document.write(3>4 || 2>3)  //False
+// document.write(4<10 || 25<=24)  //True
+
+
+// let num=5
+
+// let num=+prompt("Enter value of num")
+
+// // num=6
+// // num=num+2
+// // num+=2
+
+// document.write(num)
+// num=num+3
+
+
+// let num=+prompt("Enter value of num")
+
+// // num%=4
+// // num=num%4
+
+// document.write(num)
+
+// const pi=22/7
+// let radius=prompt("Enter value of radius")
+// let area=pi*(parseFloat(radius)*parseFloat(radius))
+// document.write("The area of circle is: "+"<br>"+area.toFixed())
+
+
+// const pi=22/7
+// let radius=+prompt("Enter radius")
+// let height=prompt("Enter height")
+// let volume=pi*(radius**2)*parseFloat(height)
+// document.write("The volume of cylinder is: "+volume)
+
+// let radius=+prompt("Enter radius")
+// let power=(radius**10)
+// document.write(power) 
+
+// let item=prompt("Kia laye ho?")
+
+
+// if(item==="tomato")
+// {
+//     document.write("Shabash")
+// }
+// else
+// {
+//     document.write("Dobara ghar nh ana")
+// }
+
+// let age=+prompt("Enter your age")
+
+// if(age>=18){
+//     document.write("You are eligible to vote")
+// }
+// else{
+//     document.write("You are not eligible to vote")
+// }
+
+// let age=prompt("Enter your age")
+// let country=prompt("Please enter your country")
+// if(parseFloat(age)>=18 && country==="pakistan"){
+//     document.write("You are eligible to vote")
+// }
+// else{
+//     document.write("You are not eligible to vote")
+// }
+
+
+// let age=prompt("Enter your age")
+// let country=prompt("Please enter your country")
+// if(parseFloat(age)>=18 || country==="pakistan"){
+//     document.write("You are eligible to vote")
+// }
+// else{
+//     document.write("You are not eligible to vote")
+// }
+
+let email=prompt("Enter your email")
+let password=prompt("Enter password")
+
+if(email==="someone@example.com" && password==="abc123"){
+    document.write("Login Successful")
+}
+else{
+    document.write("Login Failed")
+}
+
+
+
